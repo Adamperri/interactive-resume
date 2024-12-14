5ab2b689c47f7ec24e5f11e2d8348e04d102def1
AdamPerri <adamperri12@gmail.com>
2024-12-13 19:54:45 -0700

    Resume progess v0.4

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
 // script.js
 
 document.addEventListener("DOMContentLoaded", () => {
-    const buttons = document.querySelectorAll(".btn");
     const hiddenElements = document.querySelectorAll(".hidden");
     let animationTriggered = false;
-  
-    // Add sequential bounce effect to each letter in buttons
-    buttons.forEach((button) => {
-      const text = button.textContent;
-      button.textContent = ""; // Clear original text
-      text.split("").forEach((letter, index) => {
-        const span = document.createElement("span");
-        span.textContent = letter;
-        span.style.setProperty("--letter-index", index);
-        button.appendChild(span);
-      });
-    });
   
     // Trigger animations when user moves the mouse for the first time
     const showElements = () => {
