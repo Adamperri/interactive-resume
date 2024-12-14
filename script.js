1b8fc8d81d445627ccfe79ba3dcbb14cb0bf5bb0
AdamPerri <adamperri12@gmail.com>
2024-12-13 19:08:22 -0700

    Resume progess v0.2

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+// script.js
+
+// Mouse Effect
 document.addEventListener("mousemove", (e) => {
     const mouseEffect = document.querySelector(".mouse-effect");
-    mouseEffect.style.transform = `translate(${e.pageX}px, ${e.pageY}px)`;
+    const x = e.pageX;
+    const y = e.pageY;
+    mouseEffect.style.transform = `translate(${x - 25}px, ${y - 25}px)`; // Offset to center the effect on the cursor
   });
+  
+  // Smooth Scroll for "About Me" Link
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute("href"));
+      target.scrollIntoView({ behavior: "smooth" });
+    });
+  });
+  
