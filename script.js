e711e9ccf38b8906ddd265b5bd5128c556f8761a
AdamPerri <adamperri12@gmail.com>
2024-12-13 20:50:43 -0700

    Resume progess v0.5

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-// script.js
-
 document.addEventListener("DOMContentLoaded", () => {
     const hiddenElements = document.querySelectorAll(".hidden");
+    const nameText = document.getElementById("name-text");
+    const taglineText = document.getElementById("tagline-text");
     let animationTriggered = false;
+  
+    // Add hover effects to Hero section text
+    const addHoverEffects = (element) => {
+      const text = element.textContent.split(" ");
+      element.textContent = "";
+      text.forEach((word) => {
+        const wordContainer = document.createElement("span");
+        wordContainer.style.marginRight = "0.25em"; // Space between words
+        word.split("").forEach((letter) => {
+          const span = document.createElement("span");
+          span.textContent = letter;
+          wordContainer.appendChild(span);
+        });
+        element.appendChild(wordContainer);
+      });
+    };
+  
+    addHoverEffects(nameText);
+    addHoverEffects(taglineText);
   
     // Trigger animations when user moves the mouse for the first time
     const showElements = () => {
